7c8bf3555af254b277e79e600886742554c4f51b
Xiaofei Wang <xiaofeilovehuahua@gmail.com>
2020-02-26 17:32:36 -0500

    Add files via upload

---
+++ Exception Handling/Java Exception Handling (Try-catch)/solution.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println(sc.nextInt() / sc.nextInt());
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException ex) {
+            System.out.println(ex);
+        }
+    }
+}
